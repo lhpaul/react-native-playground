028311ddcfdb57e29a09e96d8c14615c910393ec
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-05-16 17:11:58 -0400

    chore: Monorepo working react native and web app sharing ui package.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+This document is to keep track of the technical debt in this repository.
+
+- Prepare environment (https://reactnative.dev/docs/set-up-your-environment)
---
+++ packages/configs/jest/base.ts
@@ -0,0 +1,32 @@
+import type { Config } from "jest";
+
+export const config = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/index.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.constants.ts",
+    "!src/**/*.mocks.ts",
+    "!src/**/*.interfaces.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["js", "ts", "json"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  preset: "ts-jest/presets/js-with-ts-esm",
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+} as const satisfies Config;
